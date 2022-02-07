18e6537deaac5a2a2e4632256bbea10f01044c6d
dianKoliva <dianpretty01@gmail.com>
2022-02-07 19:53:34 +0200

    functions

---
+++ functions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+	fmt.Printf("morning %v \n", n)
+}
+
+//we can also pass functions to funcs
+
+func cycle(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
+	}
+}
+
+func area(r float64) float64 {
+	return math.Pi * r * r
+}
+
+func main() {
+	// sayGreeting("Pretty")
+	// cycle([]string{"cloud", "tifa", "mummy"}, sayGreeting)
+	a1 := area(10.5)
+	fmt.Println(a1)
+
+}
