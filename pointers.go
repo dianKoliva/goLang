3392e17d49e53d87803e9628408a47ac216fd519
dianKoliva <dianpretty01@gmail.com>
2022-02-10 19:20:12 +0200

    new changes

--- pointers.go
+++ pointers.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
+func upName(x *string) {
+
+	*x = "huhho"
+
+}
+
 func main() {
 	name := "pp"
 	memo := &name
-	fmt.Printf("memory : %v value :%v", memo, *memo)
+	// fmt.Printf("memory : %v value :%v", memo, *memo)
+	upName(memo)
+	fmt.Println(name)
 }
